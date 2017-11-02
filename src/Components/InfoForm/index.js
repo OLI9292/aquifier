d21bd6d0c096fb70f9e5b304afd76ff0f7fbc202
Oliver Plunkett <otplunkett@gmail.com>
2017-11-02 08:09:14 -0400

    change to dateString

--- src/Components/InfoForm/index.js
+++ src/Components/InfoForm/index.js
@@ -35,7 +35,7 @@ class InfoForm extends Component {
     let obj = {};
     this.state.smallInputs.forEach((i) => obj[i.name] = i.value );
     obj.comments = this.state.comments;
-    obj.timestamp = Date.now();
+    obj.timestamp = Date.now().toDateString();
     return obj;
   }
 
