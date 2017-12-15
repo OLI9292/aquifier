8b5a8bf655ba9c20fbc1c4b477123b90ef2be214
Oliver Plunkett <otplunkett@gmail.com>
2017-12-15 14:14:29 +0100

    cleanup

--- src/Models/Class.js
+++ src/Models/Class.js
@@ -1,24 +1,15 @@
 import axios from 'axios';
 import CONFIG from '../Config/main';
 
-const forTeacher = async (id) => {
-  try {
-    return await axios.get(`${CONFIG.ACCOUNTS_API}/class?teacher=${id}`);
-  } catch (e) {
-    return e.response.data;
-  }
-}
-
 const studentsInClass = async (id) => {
   try {
-    return await axios.get(`${CONFIG.ACCOUNTS_API}/class/${id}/students`);
+    return await axios.get(`${CONFIG.ACCOUNTS_API}/v2/auth/class/${id}/students`);
   } catch (e) {
     return e.response.data;
   }
 }
 
 const Class = {
-  forTeacher: forTeacher,
   students: studentsInClass
 }
 
